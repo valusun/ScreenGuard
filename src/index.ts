9d411b5951cc30e4ba89bed31077a9c37109784d
valusun <chobicoco0327@gmail.com>
2023-08-12 15:59:52 +0900

    :tada: add 実装

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow () {
+  const options: Electron.BrowserWindowConstructorOptions = {
+    movable: false,
+    minimizable: false,
+    resizable: false,
+    titleBarStyle: 'hidden',  // タイトルバーをダブルクリックすると最大化が解除されるため隠しておく
+    backgroundColor: "#000",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  }
+  const win = new BrowserWindow(options);
+  // win.loadFile('src/index.html');
+  win.maximize();
+}
+
+app.whenReady().then(createWindow);
